104d928c4e1d9c710926c4f85512fb9cdba59669
Brad Chao <brad@brad.tw>
2017-11-12 13:50:10 +0800

    201711121350

--- app/src/main/java/com/example/administrator/mybletest1/BLEService.java
+++ app/src/main/java/com/example/administrator/mybletest1/BLEService.java
@@ -2,6 +2,9 @@ package com.example.administrator.mybletest1;
 
 import android.app.Service;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
@@ -9,6 +12,32 @@ import android.os.IBinder;
 public class BLEService extends Service {
     private final LocalBinder mBinder = new LocalBinder();
     private BluetoothDevice device;
+    private final BluetoothGattCallback mGattCallback = new BluetoothGattCallback() {
+        @Override
+        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+            super.onConnectionStateChange(gatt, status, newState);
+        }
+
+        @Override
+        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+            super.onServicesDiscovered(gatt, status);
+        }
+
+        @Override
+        public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+            super.onCharacteristicRead(gatt, characteristic, status);
+        }
+
+        @Override
+        public void onCharacteristicWrite(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+            super.onCharacteristicWrite(gatt, characteristic, status);
+        }
+
+        @Override
+        public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
+            super.onCharacteristicChanged(gatt, characteristic);
+        }
+    };
 
     public class LocalBinder extends Binder {
         BLEService getService(){return BLEService.this;}
@@ -18,7 +47,8 @@ public class BLEService extends Service {
     public IBinder onBind(Intent intent) {
         device = (BluetoothDevice) intent.getParcelableExtra("device");
 
-        // 
+        //
+        device.connectGatt(this, false, mGattCallback);
 
 
         return mBinder;
