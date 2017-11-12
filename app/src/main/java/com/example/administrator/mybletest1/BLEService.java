2d50a424d38b688b6fc8fa3425792dc4c539c0ae
Brad Chao <brad@brad.tw>
2017-11-12 13:39:39 +0800

    201711121339

--- app/src/main/java/com/example/administrator/mybletest1/BLEService.java
+++ app/src/main/java/com/example/administrator/mybletest1/BLEService.java
@@ -1,24 +1,26 @@
 package com.example.administrator.mybletest1;
 
 import android.app.Service;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
 
 public class BLEService extends Service {
     private final LocalBinder mBinder = new LocalBinder();
+    private BluetoothDevice device;
 
     public class LocalBinder extends Binder {
         BLEService getService(){return BLEService.this;}
     }
 
     @Override
-    public void onCreate() {
-        super.onCreate();
-    }
-
-    @Override
     public IBinder onBind(Intent intent) {
+        device = (BluetoothDevice) intent.getParcelableExtra("device");
+
+        // 
+
+
         return mBinder;
     }
 
--- app/src/main/java/com/example/administrator/mybletest1/ConnectActivity.java
+++ app/src/main/java/com/example/administrator/mybletest1/ConnectActivity.java
@@ -1,11 +1,33 @@
 package com.example.administrator.mybletest1;
 
 import android.bluetooth.BluetoothDevice;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class ConnectActivity extends AppCompatActivity {
     private BluetoothDevice device;
+    private BLEService mService;
+    private boolean isConnect;
+    private ServiceConnection mConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            BLEService.LocalBinder binder =
+                    (BLEService.LocalBinder)iBinder;
+            mService = binder.getService();
+            isConnect = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            isConnect = false;
+        }
+    } ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +37,25 @@ public class ConnectActivity extends AppCompatActivity {
         device = (BluetoothDevice) getIntent().getParcelableExtra("device");
 
 
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Intent it = new Intent(this, BLEService.class);
+        it.putExtra("device", device);
+        bindService(it, mConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (isConnect){
+            unbindService(mConnection);
+        }
 
     }
 }
